ecf7782f288c274f3cb4e9aeb7eebc434874eefb
Dinara <ismanalievadina@gmail.com>
2022-06-11 16:15:20 +0300

    Events.Js

---
+++ script.js
@@ -0,0 +1,14 @@
+let button = document.querySelector('#btn')
+console.log(button);
+button.style.backgroundColor = 'magenta',
+button.style.borderRadius = '10px'
+
+
+
+function changeBgPink(event){
+   document.body.style.backgroundColor = 'pink'
+   button.style.backgroundColor = 'pink'
+}
+function changeBgGray(event){
+   document.body.style.backgroundColor = 'gray'
+}
